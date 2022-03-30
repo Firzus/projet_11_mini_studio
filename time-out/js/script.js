2481f8f054df61def02dd36e4743bd83bd20efa0
zbie <ejoachimgabin@gaming.tech>
2022-03-30 21:37:00 +0200

    player movement

--- time-out/js/script.js
+++ time-out/js/script.js
@@ -2,18 +2,30 @@ var config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
+        update: update,
     }  
 };
 
 var game = new Phaser.Game(config);
 
+
 function preload() {
     this.load.image('start_map', 'assets/map.png'); 
     this.load.image('player1', 'assets/player1.png');
+    this.load.image('player2', 'assets/player2.png');
+    this.load.image('dude', 'assets/player1.png' );
 }
+var player;
+var platforms;
 
 function create() {
     this.add.image(960, 540, 'start_map');
@@ -24,41 +36,93 @@ function create() {
     r2.setStrokeStyle(2, 0x008000);
     r3.setStrokeStyle(2, 0xff0000); 
 
-    //création du sprite
-    //var sprite = game.add.sprite(640, 360, 'player');
-    const sprite = this.add.image(960, 540, 'player1');         
-
-
+    /*création du sprite
+    var x=400;
+    var y=400;
+    var sprite = this.add.sprite(x, y, 'player1');
+    //variable de déplacement du joueurs
+    var cursors = this.input.keyboard.createCursorKeys();
     //taille du joueur
     sprite.widht = 60
     sprite.height = 60
     //changer le pooint d'encrage
     sprite.anchor.setTo(0.5, 0.9);
     //modifier sa position
-    sprite.positioon.setToo(x, y);
+    sprite.positioon.setToo(x, y);*/
+    
+    cursors = this.input.keyboard.createCursorKeys();
+    player = this.physics.add.sprite(100, 450, 'dude');
 
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
-
-    var zKey = game.input.keyboard.addKey(Phaser.Keyboard.Z);
-    var sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
-    var dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
-    var qKey = game.input.keyboard.addKey(Phaser.Keyboard.Q);
 }
 
 function update() {
 
-    //enregistrement d'un évènement du clavier
+    /*enregistrement d'un évènement du clavier
 
-    if (zkey.isDown) {
-        sprite.y++
+    if (cursors.up.isDown) {
+        sprite.y = 500;
+        x=500;sz
     }
-    if (skey.isDown) {
-        sprite.y--
+    if (cursors.right.isDown) {
+        sprite.x = 500;
     }
-    if (dkey.isDown) {
-        sprite.x++
+    if (cursors.down.isDown) {
+        y--;
     }
-    if (qkey.isDown) {
-        sprite.x--
+    if (cursors.left.isDown) {
+        y--;
+    }*/
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+        player('player2');
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+    }
+    else if (cursors.up.isDown)
+    {
+        player.setVelocityY(-160);
+    }
+    else if (cursors.down.isDown)
+    {
+        player.setVelocityY(160);
+    }
+
+    /*else if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+        player.setVelocityY(-160);            
+    }
+    else if (cursors.right.isDown && cursors.up.isDown)
+    {
+        player.setVelocityX(160);
+        player.setVelocityY(-160);            
+    }
+    else if (cursors.left.isDown && cursors.down.isDown)
+    {
+        player.setVelocityX(-160);
+        player.setVelocityY(160);
+    }
+    else if (cursors.right.isDown && cursors.down.isDown)
+    {
+        player.setVelocityX(160);
+        player.setVelocityY(160);
+    }*/
+
+    else
+    {
+        player.setVelocityX(0);
+        player.setVelocityY(0);
+    }
+    
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
     }
 }
